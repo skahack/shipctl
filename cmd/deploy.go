75e044656b586f68b32c6759d4eb78064d9ce90d
Hiroki Sato <m@skahack.com>
2017-04-05 21:08:36 +0900

    refactor logger

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -329,71 +329,14 @@ func tagDockerImage(ecrClient *ecr.ECR, repoName string, fromTag string, toTag s
 	return nil
 }
 
-type logger struct {
-	Cluster         string
-	ServiceName     string
-	Out             io.Writer
-	SlackWebhookUrl string
-}
-
-func NewLogger(cluster, serviceName, slackWebhookUrl string, out io.Writer) *logger {
-	return &logger{
-		Cluster:         cluster,
-		ServiceName:     serviceName,
-		SlackWebhookUrl: slackWebhookUrl,
-		Out:             out,
-	}
-}
-
-func (l *logger) log(message string) {
-	if l.Out != nil {
-		fmt.Fprintf(l.Out, message)
+func getAWSRegion() string {
+	if os.Getenv("AWS_REGION") != "" {
+		return os.Getenv("AWS_REGION")
 	}
 
-	if l.SlackWebhookUrl != "" {
-		client := &slack.Client{WebhookURL: l.SlackWebhookUrl}
-		payload := &slack.Payload{
-			Username: "deploy-bot",
-			Text:     fmt.Sprintf("cluster: %s, serviceName: %s\n%s", l.Cluster, l.ServiceName, message),
-		}
-		client.Post(payload)
-	}
-}
-
-func (l *logger) logWithType(message string, messageType string) {
-	if messageType == "" {
-		l.log(message)
-		return
+	if os.Getenv("AWS_DEFAULT_REGION") != "" {
+		return os.Getenv("AWS_DEFAULT_REGION")
 	}
 
-	if l.Out != nil {
-		fmt.Fprintf(l.Out, message)
-	}
-
-	if l.SlackWebhookUrl != "" {
-		color := func() string {
-			if messageType == "danger" {
-				return "danger"
-			}
-			return "good"
-		}()
-		client := &slack.Client{WebhookURL: l.SlackWebhookUrl}
-		attachment := &slack.Attachment{
-			Color: color,
-			Text:  fmt.Sprintf("cluster: %s, serviceName: %s\n%s", l.Cluster, l.ServiceName, message),
-		}
-		payload := &slack.Payload{
-			Username:    "deploy-bot",
-			Attachments: []*slack.Attachment{attachment},
-		}
-		client.Post(payload)
-	}
-}
-
-func (l *logger) success(message string) {
-	l.logWithType(message, "good")
-}
-
-func (l *logger) fail(message string) {
-	l.logWithType(message, "danger")
+	return ""
 }
